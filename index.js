9bcb10c5699297ee1b4b5341408fe21478aeb3fd
Dmitriy Serdyukov <dmitriy@rpmsoftware.com>
2016-01-19 15:38:08 -0700

    1st commit

---
+++ index.js
@@ -0,0 +1,90 @@
+/* global process */
+/* global Buffer */
+var amqplib = require('amqplib');
+
+var DEFAULT_URL = process.env['CLOUDAMQP_URL'] || 'amqp://localhost';
+
+function Sender(channel, queue) {
+    this.channel = channel;
+    this.queue = queue;
+    channel.assertQueue(queue, { durable: true });
+    console.log('Sender created. Queue: %s', queue);
+
+}
+
+Sender.prototype.close = function (closeConnection) {
+    if (this.channel) {
+        var self = this;
+        return self.channel.close().then(function(){
+            var ch = self.channel; 
+            self.channel = undefined;
+            if(closeConnection) {
+                return ch.connection.close();
+            }
+        });
+    }
+};
+
+Sender.prototype.send = function (data) {
+    if (!Buffer.isBuffer(data)) {
+        if (typeof data !== 'string') {
+            data = JSON.stringify(data);
+        }
+        data = new Buffer(data);
+    }
+    return this.channel.sendToQueue(this.queue, data, { persistent: true });
+
+};
+
+function createChannel(url) {
+    var conn;
+    console.log('AMQP. Connecting to ',url);
+    return amqplib.connect(url)
+        .then(function (connect) {
+            conn = connect;
+            return connect.createChannel();
+        })
+        .then(function (channel) {
+            console.log('Channel created. URL: %s', url);
+            return channel;
+        },
+        function (error) {
+            if(conn) {
+                conn.close();
+            }
+            throw error;
+        });
+}
+
+exports.createSender = function (queue, url) {
+    url = url || DEFAULT_URL;
+    return createChannel(url).then(function (channel) {
+        return new Sender(channel, queue);
+    });
+};
+
+exports.createReciever = function (queue, url, callback) {
+    if (typeof url === 'function') {
+        callback = url;
+        url = undefined;
+    }
+    url = url || DEFAULT_URL;
+    return createChannel(url).then(function (channel) {
+        channel.assertQueue(queue, { durable: true });
+        channel.prefetch(1);
+        channel.consume(queue, function (msg) {
+            var data = msg.content.toString();
+            try {
+                data = JSON.parse(data);
+            } catch (error) {
+            }
+            if (callback) {
+                callback(data, msg, channel);
+            }
+            channel.ack(msg);
+        }, { noAck: false });
+        console.log('Listening');
+        return channel;
+    });
+};
+
