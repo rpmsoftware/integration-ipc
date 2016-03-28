aeb6890b788b5c935299c0a5e70c1dac3a6df6df
Dmitriy Serdyukov <dmitriy@rpmsoftware.com>
2016-03-28 11:09:06 -0600

    Non-destructive extension

--- memcache.js
+++ memcache.js
@@ -1,35 +1,32 @@
 var memJs = require('memjs');
 var Client = memJs.Client;
 
-var originalGet = Client.prototype.get;
 
-Client.prototype.get = function (key) {
-    var core = originalGet.bind(this);
+Client.prototype.getPromised = function (key) {
+    var self = this;
     return new Promise(function (resolve, reject) {
-        core(key, function (err, val) {
+        self.get(key, function (err, val) {
             err ? reject(err) : resolve(JSON.parse(val));
         });
     });
 };
 
-var originalSet = Client.prototype.set;
 
-Client.prototype.set = function (key, val) {
-    var core = originalSet.bind(this);
+Client.prototype.setPromised = function (key, val) {
+    var self = this;
     return new Promise(function (resolve, reject) {
         val = JSON.stringify(val);
-        core(key, val, function (err, success) {
+        self.set(key, val, function (err, success) {
             err ? reject(err) : resolve(success);
         });
     });
 };
 
-var originalFlush = Client.prototype.flush;
 
-Client.prototype.flush = function () {
-    var core = originalFlush.bind(this);
+Client.prototype.flushPromised = function () {
+    var self = this;
     return new Promise(function (resolve, reject) {
-        core(function (err, success) {
+        self.flush(function (err, success) {
             err ? reject(err) : resolve(success);
         });
     });
