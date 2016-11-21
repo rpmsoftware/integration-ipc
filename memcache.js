7bead7b3f89b82e286a17d9dd7da19765e0e0e26
Dmitriy Serdyukov <dmitriy@rpmsoftware.com>
2016-11-21 13:51:53 -0700

    Delete if empty value

--- memcache.js
+++ memcache.js
@@ -4,18 +4,19 @@ var Client = memJs.Client;
 
 Client.prototype.getPromised = function (key) {
     var self = this;
-    return new Promise((resolve, reject) => self.get(key, (err, val) => err ? reject(err) : resolve(JSON.parse(val))));
+    return new Promise((resolve, reject) => self.get(key, (err, val) => err ? reject(err) : resolve(val && JSON.parse(val))));
 };
 
 
 Client.prototype.setPromised = function (key, val) {
     var self = this;
     return new Promise((resolve, reject) => {
-        val = JSON.stringify(val);
-        self.set(key, val, (err, success) => err ? reject(err) : resolve(success));
+        function cb(err, success) {
+            err ? reject(err) : resolve(success);
+        }
+        val ? self.set(key, JSON.stringify(val), cb) : self.delete(key, cb);
     });
 };
-
 
 Client.prototype.flushPromised = function () {
     var self = this;
