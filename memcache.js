3179d78ea0f84ff3d448d3f4020032949659334c
Dmitriy Serdyukov <dmitriy@rpmsoftware.com>
2017-01-13 09:48:57 -0700

    Added Client.cachedInit()

--- memcache.js
+++ memcache.js
@@ -1,7 +1,6 @@
 var memJs = require('memjs');
 var Client = memJs.Client;
 
-
 Client.prototype.getPromised = function (key) {
     var self = this;
     return new Promise((resolve, reject) => self.get(key, (err, val) => err ? reject(err) : resolve(val && JSON.parse(val))));
@@ -23,8 +22,32 @@ Client.prototype.flushPromised = function () {
     return new Promise((resolve, reject) => self.flush((err, success) => err ? reject(err) : resolve(success)));
 };
 
+const MEMCACHE_PROPERTY = 'memcache';
 
-for (var key in memJs) {
-    exports[key] = memJs[key];
+var hash;
+function getHash(value) {
+    if (!hash) {
+        hash = require('object-hash');
+    }
+    return hash(value);
 }
+
+Client.prototype.cachedInit = function (config, cbInit) {
+    var key = config[MEMCACHE_PROPERTY];
+    if (!key) {
+        return cbInit(config);
+    }
+    var checksum = getHash(config);
+    var self = this;
+    return self.getPromised(key).then(cached => {
+        if (cached && cached.checksum === checksum) {
+            return Promise.resolve(cached.value);
+        }
+        var value = cbInit(config);
+        return (value instanceof Promise ? value : Promise.resolve(value)).then(value =>
+            self.setPromised(key, { checksum, value }).then(() => value));
+    });
+}
+
+module.exports = memJs;
 
